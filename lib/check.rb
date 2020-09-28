2d9778bd7f9b4a478372f4777d3736967e4c2be8
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-28 14:10:24 -0500

    Add a new method for tag errors.

--- lib/check.rb
+++ lib/check.rb
@@ -3,5 +3,21 @@ require 'strscan'
 require_relative 'file_reader.rb'
 
 class CheckError
-  
+  attr_reader :errors, :checker
+
+  def initialize(file_path)
+    @errors = []
+    @checker = FileReader.new(file_path)
+
+    @keywords = %w[module unless do if case class begin]
+  end
+
+  def tag_error
+    check_tag_error(/\(/, /\)/, '(', ')', 'Parentheses')
+    check_tag_error(/\[/, /\]/, '[', ']', 'Square_Brackets')
+    check_tag_error(/\{/, /\}/, '{', '}', 'Curly_Brackets')  
+  end
+
+
+
 end
