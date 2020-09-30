3a5f6b0147f2e21b36fe5d81c0a56b14906312b1
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-29 21:49:49 -0500

    Fixed issues in check_speck file.

--- spec/check_spec.rb
+++ spec/check_spec.rb
@@ -1,40 +1,26 @@
 require_relative '../lib/check.rb'
 
 describe CheckError do
-  let(:checker) { CheckError.new('bug.rb') }
-  
-  describe '#check_trailing_spaces' do
-    it 'should return trailing space error on line 3' do
-      checker.check_trailing_spaces
-      expect(checker.errors[0]).to eql('line:3:20: Error: Trailing whitespace detected.')
-    end
-  end
-  
-  describe '#check_indentation' do
-    it 'should return indentation space error on line 4' do
-      checker.check_indentation
-      expect(checker.errors[0]).to eql('line:4 IndentationWidth: Use 2 spaces for indentation.')
-    end
-  end
-  
+  let(:checker) { CheckError.new('../bugs/bug.rb') }
+
   describe '#tag_error' do
     it "returns missing/unexpected tags eg '( )', '[ ]', and '{ }'" do
       checker.tag_error
-      expect(checker.errors[0]).to eql("line:3 Lint/Syntax: Unexpected/Missing token ']' Square Bracket")
+      expect(checker.errors[0]).to eq("line:3 Lint/Syntax: Unexpected/Missing token ']' Square Bracket")
     end
   end
 
-    describe '#end_error' do
-      it 'returns missing/unexpected end' do
-        checker.end_error
-        expect(checker.errors[0]).to eql("Lint/Syntax: Missing 'end'")
-      end
+  describe '#end_error' do
+    it 'returns missing/unexpected end' do
+      checker.end_error
+      expect(checker.errors[0]).to eq("Lint/Syntax: Missing 'end'")
     end
+  end
 
   describe '#empty_line_error' do
     it 'returns empty line error' do
       checker.empty_line_error
-      expect(checker.errors[0]).to eql('line:11 Extra empty line detected at block body end')
+      expect(checker.errors[0]).to eq('line:11 Extra empty line detected at block body end')
     end
   end
 end
